6ff21c63cfed9c39dbeef36a7a1e3d55873014cf
Filippo Campegiani <filippo.campegiani@posteitaliane.it>
2021-03-22 17:21:14 +0100

    first commit

---
+++ myapp.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/filippocam/filonummanip/calc"
+	calcNew "github.com/filippocam/filonummanip/v2/calc"
+	"log"
+)
+
+func main() {
+	result := calc.Add(1)
+	fmt.Println("old somma: ", result)
+	calc.Echo("ciao", "ciao2")
+
+	err, result := calcNew.Add(1,2,3,4)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("new somma: ", result)
+
+	calcNew.Echo("ciao")
+}
